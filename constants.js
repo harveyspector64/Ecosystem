fa379ce8d57eef9a7009dca8f9f0b5c0db85e04d
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-15 13:22:57 -0700

    Update constants.js

--- constants.js
+++ constants.js
@@ -5,10 +5,12 @@ const EMOJIS = {
     TREE: '🌳',
     BUTTERFLY: '🦋',
     BIRD: '🐦',
-    WORM: '🐛'
+    WORM: '🐛'  // Using caterpillar emoji to represent worms
 };
 
 const INITIAL_EMOJIS = [
     { id: 'flowering-bush', emoji: EMOJIS.BUSH },
     { id: 'tree', emoji: EMOJIS.TREE, disabled: true }
 ];
+
+export { EMOJIS, INITIAL_EMOJIS };
